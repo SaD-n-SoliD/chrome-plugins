f8510703d56669b36b844b6659c7ba116052fba5
SaD-n-SoliD <yodas-worker@yandex.ru>
2023-02-19 00:03:37 +0300

    flipping back using browser history

--- universal-manga-flipping-plugin/content.js
+++ universal-manga-flipping-plugin/content.js
@@ -1,6 +1,7 @@
 console.log('universal manga flipping hotkey extension is working!');
 console.log('<- -> to flip through chapters');
 console.log('[Insert] then [<-] or [->] then [click] to prev/next button to save hotkey');
+console.log('use [rightClick] instead of [click] if you want to flip back using browser history');
 let lastKey, newNextBtnState, newPrevBtnState, timeout
 const hostname = document.location.hostname
 const storage = chrome.storage.local
@@ -26,23 +27,35 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('click', async (e) => {
 	if (!newNextBtnState && !newPrevBtnState) return
 	e.preventDefault()
-	let prev, next
+	let prev, next, browserPrev
 	let curr = (await storage.get([hostname]))[hostname] || {}
 	if (newNextBtnState) {
 		prev = curr.prev
 		next = getElementImage(e.target)
+		browserPrev = curr.browserPrev
 	} else if (newPrevBtnState) {
 		prev = getElementImage(e.target)
 		next = curr.next
+		browserPrev = false
 	}
-	console.log('new', { prev, next });
+	console.log('new', { prev, next, browserPrev });
 	await storage.set({
-		[hostname]: { prev, next }
+		[hostname]: { prev, next, browserPrev }
 	})
 	newNextBtnState = newPrevBtnState = false
 	clearTimeout(timeout)
 })
 document.addEventListener('keydown', addHotkeys)
+window.addEventListener('contextmenu', async (e) => {
+	if (!newPrevBtnState) return
+	e.preventDefault()
+	let curr = (await storage.get([hostname]))[hostname] || {}
+	await storage.set({
+		[hostname]: { ...curr, browserPrev: true }
+	})
+	newNextBtnState = newPrevBtnState = false
+	clearTimeout(timeout)
+});
 
 function clickBtn(btn) {
 	btn.dispatchEvent(new Event('click', { bubbles: true }))
@@ -58,15 +71,18 @@ function getElementByImage(image) {
 	console.log('elByImage ', image.selector, el);
 	return el
 }
+function moveToPrevChapter(image, browserPrev) {
+	browserPrev ? history.back() : clickBtn(getElementByImage(image))
+}
 async function addHotkeys(e) {
 	if (e.ctrlKey || e.shiftKey || e.altKey || !['ArrowLeft', 'ArrowRight'].includes(e.code) || newPrevBtnState || newNextBtnState) return
-	let { prev, next } = (await storage.get([hostname]))[hostname] || {}
+	let { prev, next, browserPrev } = (await storage.get([hostname]))[hostname] || {}
 	console.log('using hotkey', prev, next);
 	if (!next) return
 
 	switch (e.code) {
 		case 'ArrowLeft':
-			clickBtn(getElementByImage(prev))
+			moveToPrevChapter(prev, browserPrev)
 			break
 		case 'ArrowRight':
 			clickBtn(getElementByImage(next))
